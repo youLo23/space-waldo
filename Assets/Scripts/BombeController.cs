1b9c54e21f82c7418f2539d72f33f0ebecb186f3
Chenggen <chenggen88@hotmail.com>
2015-12-07 17:52:01 +0100

    nv script Bombe

---
+++ Assets/Scripts/BombeController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombeController : MonoBehaviour {
+
+	private bool bombe;
+	private int cpt;		// Bombe reste
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		cpt = 3;
+		bombe = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bombe) {
+			if(Input.GetKeyDown(KeyCode.B)){
+				explosion();
+				cpt--;
+			}
+		}
+	}
+
+	void explosion(){
+
+		if (cpt <= 0)
+			bombe = false;
+		else {
+
+
+
+		}
+	}
+
+
+
+}
