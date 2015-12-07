622779fd3476caaaedf588c5bc7a651f3b2e4e9e
LoYou23 <youss.akadiri@gmail.com>
2015-12-07 16:11:18 +0100

    surpriseBox2

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
